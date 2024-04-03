1432ce56f552d4097362e03cf5d94c65f440bce0
Juliusz Marciniak <juliusz.marciniak@decerto.com>
2024-04-03 14:48:47 +0200

    init

---
+++ Starter.java
@@ -0,0 +1,62 @@
+public class Starter {
+    public static void main(String[] args) {
+        // Zad 1.
+        // Wypisz na konsoli Hello World!
+    }
+
+    public static String conditional() {
+        // Zad 2.
+        /*
+        Wczytaj liczbę od użytkownika i jeśli jest większa niż 10 to wypisz "Większa niż 10!", a jeśli jest mniejsza to
+        wypisz "Mniejsza niż 10!"
+        */
+        return "";
+    }
+
+    public static void loopFor() {
+        // Zad 3.
+        /*
+        Wypisz "Przebieg pętli for [X]!", gdzie X to, który przebieg pętli, a liczbę przebiegów ma wprowadzić user.
+        Użyj pętli for
+        */
+    }
+
+    public static void loopWhile() {
+        // Zad 4.
+        /*
+        Wypisz "Przebieg pętli while [X]!", gdzie X to, który przebieg pętli, a liczbę przebiegów ma wprowadzić user.
+        Użyj pętli while
+        */
+    }
+
+    public static void fillList() {
+        // Zad 5.
+        /*
+        Stwórz listę, dodawaj do niej elementy (String), dopóki nie będzie wprowadzony pusty String
+        Następnie wypisz tablicę
+        */
+    }
+
+    public static void fillArray() {
+        // Zad 6.
+        /*
+        Stwórz tablicę, dodawaj do niej elementy (String), dopóki nie będzie wprowadzony pusty String
+        Następnie wypisz tablicę
+        */
+    }
+
+    public static void throwRuntimeException() {
+        // Zad 7.
+        /*
+        Zmodyfikuj zadanie 2 o obsługę warunków brzegowych, rzuć IllegalStateException z odpowiednim message
+        */
+    }
+
+    public static void throwException() {
+        // Zad 8.
+        /*
+        Zmodyfikuj zadanie 2 o obsługę warunków brzegowych, rzuć Exception z odpowiednim message
+        Złap go w metodzie main i przekształć na odpowiednią zwrotkę na konsolę
+        */
+    }
+}
